070cce729cd65fb909e5b9eeb450354f2363e434
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-12-23 21:45:15 +0300

    Improving Account Card

--- src/components/AccountCard.tsx
+++ src/components/AccountCard.tsx
@@ -16,10 +16,10 @@ const AccountCard: React.FC<AccountCardProps> = ({
   return (
     <div
       style={{ backgroundImage: `url(${backgroundImage})` }}
-      className="relative rounded-lg border border-gray-200 bg-gray-50 bg-cover bg-center p-8 dark:border-gray-700 dark:bg-gray-800 md:p-12"
+      className="relative flex h-96 max-w-screen-lg overflow-hidden rounded-2xl border bg-cover bg-center p-8 shadow-lg dark:border-gray-700 dark:bg-gray-800 md:p-12"
     >
-      <div className="absolute inset-0 rounded-lg bg-black opacity-50"></div>
-      <div className="relative z-10">
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative z-10 flex h-full flex-1 flex-col items-start justify-center">
         <h2 className="mb-2 text-3xl font-extrabold text-white">{title}</h2>
         <p className="mb-4 text-lg font-normal text-white">{description}</p>
         <Button text={buttonText} variant="primary" />
