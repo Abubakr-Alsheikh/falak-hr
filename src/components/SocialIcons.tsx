36c316130ab7dcc6110cc8dff7c03ab3066d808e
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-12-23 22:27:41 +0300

    Improving social icons

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -4,17 +4,17 @@ import { FaXTwitter } from "react-icons/fa6";
 
 const SocialIcons: React.FC = () => {
   return (
-    <div className="flex space-x-4 text-gray-700">
-      <a href="#" className="hover:text-gray-500">
+    <div className="flex text-gray-700">
+      <a href="#" className="mr-4 text-3xl text-secondary-900 transition-colors hover:text-secondary-700">
         <FaXTwitter />
       </a>
-      <a href="#" className="hover:text-gray-500">
+      <a href="#" className="mr-4 text-3xl text-secondary-900 transition-colors hover:text-secondary-700">
         <AiFillInstagram />
       </a>
-      <a href="#" className="hover:text-gray-500">
+      <a href="#" className="mr-4 text-3xl text-secondary-900 transition-colors hover:text-secondary-700">
         <AiFillYoutube />
       </a>
-      <a href="#" className="hover:text-gray-500">
+      <a href="#" className="mr-4 text-3xl text-secondary-900 transition-colors hover:text-secondary-700">
         <AiFillLinkedin />
       </a>
     </div>
