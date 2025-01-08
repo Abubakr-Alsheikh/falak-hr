ad7cda08b9b11b46787e9f0ec76950ecd0b7dc51
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2025-01-09 00:10:36 +0300

    Adding lottie package with animation svg

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,43 +1,27 @@
 import React from "react";
 import Button from "../Button";
 import { BsArrowLeft } from "react-icons/bs";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const HeroSection: React.FC = () => {
   return (
-    <section className="gradient-primary-color mx-4 mb-8 mt-10 rounded-3xl shadow-lg md:mx-0">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
-        <a
-          href="#"
-          className="mb-7 inline-flex items-center justify-between rounded-full bg-gray-100 px-1 py-1 pr-4 text-sm text-gray-700 hover:bg-gray-200 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700"
-          role="alert"
-          dir="ltr"
-        >
-          <span className="mr-3 rounded-full bg-primary-600 px-4 py-1.5 text-xs text-white">
-            جديد
-          </span>{" "}
-          <span className="text-sm font-medium">انظر الى هو جديد</span>
-          <svg
-            className="ml-2 h-5 w-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </a>
+    <section className="gradient-primary-color mx-4 mb-16 mt-10 flex flex-col items-center justify-center rounded-3xl py-8 shadow-lg sm:flex-row-reverse md:mx-0 md:py-24">
+      <DotLottieReact
+          src="https://lottie.host/eacdf887-412d-4012-9f06-7a9bdad032df/YKQtCye1FU.lottie"
+          loop
+          autoplay
+          className="w-full md:max-w-[650px]"
+        />
+      <div className="mx-auto px-4 py-8 text-right lg:px-12 lg:py-16">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white dark:text-white md:text-5xl lg:text-6xl">
           فلك للموارد البشرية
         </h1>
-        <p className="mb-8 text-lg font-normal text-gray-100 dark:text-gray-400 sm:px-16 lg:text-xl xl:px-48">
+        <p className="mb-8 text-lg font-normal text-gray-100 dark:text-gray-400 lg:text-xl">
           لادارة و تشغيل باقات و انظمة وبرامج ومشاريع العمل عن بعد عبر خدمة
           المراقبة الذاتية
         </p>
-        <div className="mb-8 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-x-4 sm:space-y-0 lg:mb-16">
-          <Button icon={<BsArrowLeft />} text="أبدا معنا" variant="primary" />
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-start sm:space-x-4 sm:space-y-0">
+          <Button icon={<BsArrowLeft />} text="أبدا معنا" variant="secondary" className="bg-white"/>
         </div>
       </div>
     </section>
