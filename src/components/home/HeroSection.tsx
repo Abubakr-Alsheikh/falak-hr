602a08c4e5349a4407540c0acf06a616c1c44ae2
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2025-01-16 21:32:46 +0300

    Improving Hero section

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -5,12 +5,12 @@ import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const HeroSection: React.FC = () => {
   return (
-    <section className="gradient-primary-color mx-4 mb-16 mt-10 flex flex-col items-center justify-center rounded-3xl py-8 shadow-lg sm:flex-row-reverse md:mx-0 md:py-24">
+    <section className="gradient-primary-color relative m-5 mb-16 mt-32 flex flex-col items-center justify-center rounded-2xl shadow-2xl md:mt-14 md:h-screen md:py-24 lg:flex-row-reverse">
       <DotLottieReact
-          src="https://lottie.host/eacdf887-412d-4012-9f06-7a9bdad032df/YKQtCye1FU.lottie"
+          src="https://lottie.host/94e3c8d1-60a4-4a05-890c-f5c2ac74981a/t7moSf8EH4.lottie"
           loop
           autoplay
-          className="w-full md:max-w-[650px]"
+          className="w-full md:max-w-[800px]"
         />
       <div className="mx-auto px-4 py-8 text-right lg:px-12 lg:py-16">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white dark:text-white md:text-5xl lg:text-6xl">
@@ -21,7 +21,7 @@ const HeroSection: React.FC = () => {
           المراقبة الذاتية
         </p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-start sm:space-x-4 sm:space-y-0">
-          <Button icon={<BsArrowLeft />} text="أبدا معنا" variant="secondary" className="bg-white"/>
+          <Button icon={<BsArrowLeft />} text="أبدا معنا" variant="secondary" className="m-0 bg-white"/>
         </div>
       </div>
     </section>
