9c02ccc9d44d7f25fd452840c517ab97990c7d6d
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2025-01-21 00:02:15 +0300

    Adding animations for the Remote page

--- src/pages/remote/sections/RemoteWorkTitle.tsx
+++ src/pages/remote/sections/RemoteWorkTitle.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import SectionTitle from "../../../components/SectionTitle";
+import FadeIn from "../../../components/animations/FadeIn";
+
 
 const RemoteWorkTitle: React.FC = () => {
     return (
-        <SectionTitle title="العمل عن بعد" center={true} isHeader={true} subtitle="نص أساسي لأي شيء تود قوله. أضف النقاط الرئيسية التي استخلصتها، أو الاقتباسات، أو الحكايات، أو حتى قصة قصيرة جدا." />
+        <FadeIn direction="up">
+            <SectionTitle
+              title="العمل عن بعد"
+              center={true}
+              isHeader={true}
+              subtitle="نص أساسي لأي شيء تود قوله. أضف النقاط الرئيسية التي استخلصتها، أو الاقتباسات، أو الحكايات، أو حتى قصة قصيرة جدا."
+            />
+        </FadeIn>
     );
 };
 
