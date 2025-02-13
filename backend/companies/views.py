3598e7ed6793d96ca8135f4efe3611e6b0a124f2
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2025-02-13 21:55:01 +0300

    Adding custom pagination

--- backend/companies/views.py
+++ backend/companies/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, pagination
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -8,26 +8,39 @@ from .models import Company
 from .serializers import CompanySerializer
 from rest_framework.filters import SearchFilter, OrderingFilter
 
+
+class CompanyPagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
-    # permission_classes = [IsAdminUserOrReadOnly]
+    permission_classes = [IsAdminUserOrReadOnly]
     filter_backends = [SearchFilter, OrderingFilter]
-    search_fields = ['name__icontains', 'address__icontains', 'contact_email__icontains', 'contact_phone__icontains']
-    ordering_fields = ['name', 'created_at']
-    ordering = ['name']
+    search_fields = [
+        "name__icontains",
+        "address__icontains",
+        "contact_email__icontains",
+        "contact_phone__icontains",
+    ]
+    ordering_fields = ["name", "created_at"]
+    ordering = ["name"]
+    pagination_class = CompanyPagination
 
     def get_queryset(self):
         return Company.objects.filter(parent_company__isnull=True)
 
-    @action(detail=True, methods=['get'])
+    @action(detail=True, methods=["get"])
     def subcompanies(self, request, pk=None):
         company = self.get_object()
         sub_companies = company.sub_companies.all()
         serializer = CompanySerializer(sub_companies, many=True)
         return Response(serializer.data)
 
-    @action(detail=True, methods=['get'])
+    @action(detail=True, methods=["get"])
     def employees(self, request, pk=None):
         company = self.get_object()
         employees = company.employees.all()
