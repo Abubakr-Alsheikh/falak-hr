21b03cc6b3056e617c7696d66d31cfc1b142ed87
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2025-06-13 21:21:32 +0300

    Refactor ContactUs page: Remove ServiceRequestPage section
    This commit removes the ServiceRequestPage section from the ContactUs page. The ServiceRequestPage was deemed unnecessary on the ContactUs page. Also updates the ceo image.

--- src/pages/public/contact/ContactUs.tsx
+++ src/pages/public/contact/ContactUs.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import ContactUsTitle from "./sections/ContactUsTitle";
 import ContactForm from "./sections/ContactForm";
 import MapSection from "./sections/MapSection";
-import ServiceRequestPage from "./sections/ServiceRequest";
 const ContactUs: React.FC = () => {
   return (
     <div className="container mx-auto mt-16">
       <ContactUsTitle />
-      <ServiceRequestPage />
       <ContactForm />
       <MapSection />
     </div>
